88e53cd63fe46f431de1b32cdad881337cbbfc55
Paul Betts <paul@paulbetts.org>
2013-05-08 13:04:56 -0700

    Add some tests

---
+++ SaveAllTheTime.Tests/Models/GitRepoOpsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using ReactiveUI;
+using Xunit;
+using Xunit.Extensions;
+using SaveAllTheTime.Models;
+
+namespace SaveAllTheTime.Tests.Models
+{
+    public class GitRepoOpsTests : IEnableLogger
+    {
+        [Theory]
+        [InlineData("https://notagithuburl.com", false)]
+        [InlineData("https://notagithuburl.com", false)]
+        [InlineData("git@bitbucket.org:birkenfeld/pygments-main", false)]
+        [InlineData("git@github.com:reactiveui/ReactiveUI.git", true)]
+        [InlineData("https://github.com/github/Akavache.git", true)]
+        [InlineData("git://github.com/github/Akavache.git", false)]
+        public void ProtocolUrlCheck(string remoteUrl, bool shouldNotBeNull)
+        {
+            var result = GitRepoOps.ProtocolUrlForRemoteUrl(remoteUrl);
+            this.Log().Info("Protocol URL: {0}", result);
+
+            Assert.Equal(shouldNotBeNull, !String.IsNullOrEmpty(result));
+        }
+    }
+}
---
+++ SaveAllTheTime.Tests/ViewModels/CommitHintViewModelTests.cs
@@ -0,0 +1,48 @@
+﻿using NSubstitute;
+using ReactiveUI;
+using SaveAllTheTime.Models;
+using SaveAllTheTime.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace SaveAllTheTime.Tests.ViewModels
+{
+    public class CommitHintViewModelTests : IEnableLogger
+    {
+        [Fact]
+        public void CantOpenWhenThereIsNoGitRepo()
+        {
+            var ops = Substitute.For<IGitRepoOps>();
+            var filename = @"C:\Foo\Bar\Baz.txt";
+
+            ops.FindGitRepo(filename).Returns(default(string));
+
+            RxApp.InUnitTestRunner();
+            var fixture = new CommitHintViewModel(filename, ops);
+
+            this.Log().Info("Protocol URL: {0}", fixture.ProtocolUrl);
+            Assert.False(fixture.Open.CanExecute(null));
+        }
+
+        [Fact]
+        public void CantOpenWhenThereIsNoRemote()
+        {
+            var ops = Substitute.For<IGitRepoOps>();
+            var filename = @"C:\Foo\Bar\Baz.txt";
+
+            ops.FindGitRepo(filename).Returns(@"C:\Foo");
+            ops.ProtocolUrlForRepoPath(@"C:\Foo").Returns(default(string));
+
+            var fixture = new CommitHintViewModel(filename, ops);
+
+            this.Log().Info("Protocol URL: {0}", fixture.ProtocolUrl);
+            Assert.False(fixture.Open.CanExecute(null));
+        }
+    }
+}
