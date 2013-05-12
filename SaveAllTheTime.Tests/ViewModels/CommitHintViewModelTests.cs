f610ca85520d5607541c536ed171626f0320e0d8
Paul Betts <paul@paulbetts.org>
2013-05-11 19:20:45 -0700

    Delete tests that aren't actually true anymore

--- SaveAllTheTime.Tests/ViewModels/CommitHintViewModelTests.cs
+++ SaveAllTheTime.Tests/ViewModels/CommitHintViewModelTests.cs
@@ -14,6 +14,8 @@ using Xunit;
 using Xunit.Extensions;
 using System.Reactive.Disposables;
 using System.Reactive.Subjects;
+using Microsoft.Reactive.Testing;
+using ReactiveUI.Testing;
 
 namespace SaveAllTheTime.Tests.ViewModels
 {
@@ -98,55 +100,6 @@ namespace SaveAllTheTime.Tests.ViewModels
 
             fixture.Dispose();
             Assert.Equal(0, subscriptionCount);
-        }
-
-        [Fact]
-        public void RefreshTheStatusWhenTheRepoChanges()
-        {
-            var ops = Substitute.For<IGitRepoOps>();
-            var filename = @"C:\Foo\Bar\Baz.txt";
-            var repoFswChange = new Subject<IList<string>>();
-
-            ops.FindGitRepo(filename).Returns(@"C:\Foo");
-            ops.ProtocolUrlForRepoPath(@"C:\Foo").Returns("https://github.com/reactiveui/reactiveui.git");
-            ops.LastCommitTime(@"C:\Foo").Returns(Observable.Return(DateTimeOffset.Now));
-
-            var watch = Substitute.For<IFilesystemWatchCache>();
-            watch.Register(null).ReturnsForAnyArgs(repoFswChange);
-
-            var vs = Substitute.For<IVisualStudioOps>();
-            var fixture = new CommitHintViewModel(filename, Substitute.For<IVisualStudioOps>(), ops, watch);
-            ops.Received(0).GetStatus(@"C:\Foo");
-            watch.Received(1).Register(@"C:\Foo\.git");
-
-            repoFswChange.OnNext(new List<string> { @"C:\Foo\.git\index.lock", });
-
-            ops.Received(1).GetStatus(@"C:\Foo");
-        }
-
-        [Fact]
-        public void StatusGoesFromRedToGreenWhenCommitsChange()
-        {
-            var ops = Substitute.For<IGitRepoOps>();
-            var filename = @"C:\Foo\Bar\Baz.txt";
-            var repoFswChange = new Subject<IList<string>>();
-
-            ops.ApplicationStartTime.Returns(RxApp.MainThreadScheduler.Now);
-            ops.FindGitRepo(filename).Returns(@"C:\Foo");
-            ops.ProtocolUrlForRepoPath(@"C:\Foo").Returns("https://github.com/reactiveui/reactiveui.git");
-            ops.LastCommitTime(@"C:\Foo").Returns(Observable.Return(DateTimeOffset.MinValue), Observable.Return(DateTimeOffset.Now));
-
-            var watch = Substitute.For<IFilesystemWatchCache>();
-            watch.Register(null).ReturnsForAnyArgs(repoFswChange);
-
-            var vs = Substitute.For<IVisualStudioOps>();
-            var fixture = new CommitHintViewModel(filename, Substitute.For<IVisualStudioOps>(), ops, watch);
-
-            repoFswChange.OnNext(new List<string> { @"C:\Foo\.git\index.lock", });
-            Assert.Equal(CommitHintState.Red, fixture.HintState);
-
-            repoFswChange.OnNext(new List<string> { @"C:\Foo\.git\index.lock", });
-            Assert.Equal(CommitHintState.Green, fixture.HintState);
         }
     }
 
